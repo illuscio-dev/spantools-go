733fd3d821e873f94e16ce09d876bd1adbc008be
Billy Peake <b.peake@illuscio.com>
2020-01-07 03:14:09 -0800

    paired back setup step

--- setup.py
+++ setup.py
@@ -12,11 +12,5 @@ if __name__ == "__main__":
     config = ConfigParser()
     config.read("./setup.cfg")
 
-    lib_name = config.get("metadata", "name")
-    version = config.get("bumpversion", "current_version")
-
-    # filter packages to ones with the lib name in them
-    packages: List[str] = [p for p in find_packages() if p.startswith(lib_name)]
-
     # run setup
-    setup(version=version, packages=packages)
+    setup()
